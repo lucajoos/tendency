7dd09b2f829ebd6be046c8a6ce0bf6542ec3c1f9
Luca Joos <1a85ra7z@gmail.com>
2022-05-03 13:32:17 +0200

    docs: update @example in group.ts

--- src/functions/group.ts
+++ src/functions/group.ts
@@ -2,13 +2,19 @@ import { Parameter } from '../types/index'
 
 /**
  * Groups parameters into independent environment.
+ * Alternatively, parameters can be moved into a separate array.
  * @function
  * @param parameters {...Parameter} - Multiple parameters
  * @return {Parameter[]} - Specified parameters
  *
  * @example
  *
- *    group('a', 'b')
+ *    tendency(true, group(false, 'a', 'b'))
+ *    returns: ''
+ *
+ * Alternatively:
+ *    tendency(true, [false, 'a', 'b'])
+ *    returns: ''
  */
 const group = (...parameters: Parameter[]): Parameter[] => parameters
 export default group
