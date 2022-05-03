33444830363bfd118591c9c1acd6207fd7acc1b5
Luca Joos <1a85ra7z@gmail.com>
2022-05-03 13:44:48 +0200

    docs: update documentation of min.ts

--- src/functions/min.ts
+++ src/functions/min.ts
@@ -2,6 +2,7 @@ import { Flag, Parameter } from '../types/index'
 
 /**
  * Appends parameters if the minimum number of valid conditions are true.
+ * Parameters are also appended if the given count is exactly equal to the number of conditions.
  * @function
  * @param count {number} - Minimum number of valid conditions
  * @param parameters {...Parameter} - Multiple parameters
@@ -10,6 +11,13 @@ import { Flag, Parameter } from '../types/index'
  * @example
  *
  *    min(1, 'a', 'b')
+ *    // returns: ''
+ *
+ *    min(1, true, 'a', 'b')
+ *    // returns: 'a b'
+ *
+ *    min(1, true, true, 'a', 'b')
+ *    // returns: 'a b'
  */
 const min = (count: number, ...parameters: Parameter[]): Flag => {
   return {
