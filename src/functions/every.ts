42c1dd15002f8a11be6d803a86e6851908b42e39
Luca Joos <1a85ra7z@gmail.com>
2022-05-03 14:04:52 +0200

    docs: simplify @example of every()

--- src/functions/every.ts
+++ src/functions/every.ts
@@ -2,13 +2,14 @@ import { Parameter } from '../types/index'
 
 /**
  * Appends parameters if all conditions are true.
+ * This always refers to the current environment.
  * @function
  * @param parameters {...Parameter} - Multiple parameters
  * @return {Parameter[]} - Specified parameters
  *
  * @example
  *
- *    tendency(true, true, false, every('a', 'b'))
+ *    tendency(true, false, every('a', 'b'))
  *    returns: ''
  *
  *    tendency(true, true, every('a', 'b'))
